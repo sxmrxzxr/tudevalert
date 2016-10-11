b66a4fb52eef96b6f2fb3f0b7b101337e023ab06
sxmrxzxr <tue95606@temple.edu>
2016-10-11 20:53:33 +0000

    server responds to outside texts

--- myServ.py
+++ myServ.py
@@ -15,6 +15,17 @@ def hello_monkey():
 	from_num = request.values.get("From", None)
 	print(from_num)
 
+	if from_num not in n.subs:
+		newNumMsg = request.values.get("Body")
+
+		if (newNumMsg == "SUB"):
+			n.subs.append(newNumMsg)
+			messageBody = "Thanks for subscribing to TUDevAlert!"
+			message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
+		else:
+			messageBody = "You've reached the TUDevAlert phone number, text 'SUB' if you wish to subscribe to alerts"
+			message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
+
 	if from_num in n.admins:
 		adminAlert = request.values.get("Body")
 		messageBody = "TUDevAlert: " + str(adminAlert) + ". Avoid the area. Area is safe." 
@@ -23,7 +34,7 @@ def hello_monkey():
 			message = client.messages.create(to=num, from_=c.phone, body=messageBody)
  	else:
 		resp = twilio.twiml.Response()
-		resp.message("Hello, mobile monkey")
+		resp.message("We appreciate the thought, but only TUDev Officers are allowed to send alerts.")
 		return str(resp)
 
 if __name__ == "__main__":
