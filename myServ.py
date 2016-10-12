1fbf386fcac3154f9e9be87bfa8d4f8fe546d5d1
sxmrxzxr <tue95606@temple.edu>
2016-10-12 00:20:56 +0000

    changes i cannot recall

--- myServ.py
+++ myServ.py
@@ -8,6 +8,7 @@ import twilio.twiml
 
 app = Flask(__name__)
 client = TwilioRestClient(c.account_sid, c.auth_token)
+n.on_open()
 messageBody = " "
 
 @app.route("/", methods=['GET', 'POST'])
@@ -28,26 +29,8 @@ def main():
 		messageBody = m.not_admin
 
 	message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
-	#if from_num not in n.subs:
-
-	#	if (newNumMsg == "SUB"):
-	#		n.subs.append(newNumMsg)
-	#		messageBody = "Thanks for subscribing to TUDevAlert!"
-	#		message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
-	#	else:
-	#		messageBody = "You've reached the TUDevAlert phone number, text 'SUB' if you wish to subscribe to alerts"
-	#		message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
-
-#	if from_num in n.admins:
-#		adminAlert = request.values.get("Body")
-#		messageBody = "TUDevAlert: " + str(adminAlert) + ". Avoid the area. Area is safe." 
-#
-#		for num in n.subs:
-#			message = client.messages.create(to=num, from_=c.phone, body=messageBody)
- #	else:
-#		resp = twilio.twiml.Response()
-#		resp.message("We appreciate the thought, but only TUDev Officers are allowed to send alerts.")
-#		return str(resp)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=False)
+
+n.on_close()
