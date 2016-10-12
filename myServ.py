4daba318a78f4cfed28e5c26b4428e2c4e1f0039
sxmrxzxr <tue95606@temple.edu>
2016-10-12 18:43:02 +0000

    final changes made to alpha version, 'launching' beta tonight????

--- myServ.py
+++ myServ.py
@@ -8,29 +8,38 @@ import twilio.twiml
 
 app = Flask(__name__)
 client = TwilioRestClient(c.account_sid, c.auth_token)
-n.on_open()
+
+subs = []
+muted = []
+
+subs = n.on_open(subs, "subs.txt")
 messageBody = " "
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
 	from_num = request.values.get("From", None)
 	from_msg = request.values.get("Body")
-	print(from_num)
+	alert_flag = False
 
-	if (from_num not in n.subs):
+	if (from_num not in subs):
 		if (from_msg == "SUB"):
-			n.subs.append(from_num)
+			subs.append(from_num)
 			messageBody = m.new_num
 		else:
 			messageBody = m.not_sub
-	elif (from_num in n.admins):
+	elif (from_num in c.admins):
 		messageBody = m.build_alert(from_msg)
+		alert_flag = True
 	else:
 		messageBody = m.not_admin
 
-	message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
+	if (alert_flag):
+		for s in subs:
+			message = client.messages.create(to=s, from_=c.phone, body=messageBody)
+	else:
+		message = client.messages.create(to=from_num, from_=c.phone, body=messageBody)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=False)
 
-n.on_close()
+n.on_close(subs, "subs.txt")
